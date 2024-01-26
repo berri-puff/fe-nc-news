09620a9474cec1861d1078e12e42c8fbd0b00c53
berri-puff <jennyzhang0207@gmail.com>
2024-01-26 16:09:06 +0000

    adds styling to the user page align the icons

--- components/users/users-container.jsx
+++ components/users/users-container.jsx
@@ -1,44 +1,60 @@
-import { useContext, useEffect, useState } from "react"
-import { getAllUsers } from "../../utils/api"
-import { LoadingContext } from "../../context/loading"
-import {Spinner} from '@chakra-ui/react'
-import { Link } from "react-router-dom"
+import { useContext, useEffect, useState } from "react";
+import { getAllUsers } from "../../utils/api";
+import { LoadingContext } from "../../context/loading";
+import { Spinner, Badge, Box, Avatar } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
 
+const UsersContainer = () => {
+  const [users, setUsers] = useState([]);
+  const { isLoading, setIsLoading } = useContext(LoadingContext);
+  useEffect(() => {
+    setIsLoading(true);
+    getAllUsers().then((receivedUsers) => {
+      setUsers(receivedUsers);
+      setIsLoading(false);
+    });
+  }, []);
 
-const UsersContainer = ()=>{
-    const [users, setUsers] = useState([])
-    const {isLoading, setIsLoading} = useContext(LoadingContext)
-    useEffect(()=>{
-        setIsLoading(true)
-        getAllUsers().then((receivedUsers)=>{
-            setUsers(receivedUsers)
-            setIsLoading(false)
-        })
-    }, [])
-
-    if (isLoading) {
-        return <Spinner
-        thickness='4px'
-        speed='0.85s'
-        emptyColor='purple.50'
-        color='purple.300'
-        size='xl'
+  if (isLoading) {
+    return (
+      <Spinner
+        thickness="4px"
+        speed="0.85s"
+        emptyColor="purple.50"
+        color="purple.300"
+        size="xl"
       />
-    }
-    else {
-        return (
-        <section>
-            {users.map((user)=>{
-               return <div key={user.username}>
-               <h3><Link to={`/users/${user.username}`}>{user.username}</Link></h3>
-        <Link to={`/users/${user.username}`}><img className='profilePic'src={user.avatar_url}/></Link>
-               </div>
-            })}
-        </section>
-       
-    )  
-    }
-  
-}
+    );
+  } else {
+    return (
+      <section className="users-container">
+        {users.map((user) => {
+          return (
+            <div className="user-box" key={user.username}>
+              <Box fontWeight="semibold" padding="5px" margin="10px" size='md'>
+                <Link to={`/users/${user.username}`}>
+                  <Avatar
+                    size="2xl"
+                    src={user.avatar_url}
+                  ></Avatar>
+                </Link>
+                <Box>
+                  <Badge
+                    padding="2px"
+                    colorScheme="orange"
+                    textTransform="lowercase"
+                    fontSize="md"
+                  >
+                    <Link to={`/users/${user.username}`}>{user.username}</Link>
+                  </Badge>
+                </Box>
+              </Box>
+            </div>
+          );
+        })}
+      </section>
+    );
+  }
+};
 
-export default UsersContainer
+export default UsersContainer;
